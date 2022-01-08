0d41c1bdfc120d3e60e2a15ec4a6ed278132b5d1
Lucas Sena <lucassena.rj@gmail.com>
2022-01-08 01:10:32 +0000

    feat(context): add AuthContext.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import { AuthProvider } from "../contexts/AuthContext";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  <AuthProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </AuthProvider>
 );
 
 export default MyApp;
